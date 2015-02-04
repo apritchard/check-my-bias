024e37e8586da379687d0c133a65a0ebb8758c8e
Alex Pritchard <pritchard.alex@gmail.com>
2015-02-03 21:33:07 -0600

    Some placeholder pages while I figure out how I want to do this managed bean thing

---
+++ CheckMyBias/CheckMyBias-web/src/main/java/com/appsoil/user/UserBackingBean.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.appsoil.user;
+
+import javax.faces.bean.RequestScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author alex
+ */
+@RequestScoped
+@Named
+public class UserBackingBean {
+    
+    private String firstName;
+    private String lastName;
+
+    /**
+     * @return the firstName
+     */
+    public String getFirstName() {
+        return firstName;
+    }
+
+    /**
+     * @param firstName the firstName to set
+     */
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    /**
+     * @return the lastName
+     */
+    public String getLastName() {
+        return lastName;
+    }
+
+    /**
+     * @param lastName the lastName to set
+     */
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+}
